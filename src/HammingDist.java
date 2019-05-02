1dad6a5ff78b8289950a0b9eac4317e4e15f98aa
Timmy <timothy.luong@ou.edu>
2019-05-02 09:24:52 -0500

    Added Slider

--- src/HammingDist.java
+++ src/HammingDist.java
@@ -3,6 +3,7 @@ import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 
 public class HammingDist extends JFrame
 {
@@ -11,16 +12,27 @@ public class HammingDist extends JFrame
 	private JButton showStation = new JButton("Show Station");
 	private JButton calHD = new JButton("Calculate HD");
 	private JButton addStation = new JButton("Add Station");
+	private JSlider slider = new JSlider(1,4);
 	
 	public HammingDist()
 	{
 		initFrame();
 		setButton();
+		setSlider();
 		panel1.add(showStation);
 		panel1.add(calHD);
 		panel1.add(addStation);
+		panel1.add(slider);
 		panel0.add(panel1);	
 		this.add(panel0);	
+	}
+	
+	private void setSlider() 
+	{
+		slider.setBounds(10, 25, 200, 50);
+		slider.setPaintTicks(true);
+		slider.setMajorTickSpacing(1);
+		slider.setPaintLabels(true);
 	}
 	
 	private void setButton() 
