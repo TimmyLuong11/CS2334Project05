6192493a0283b3107557866ead35c39703219428
Timmy <timothy.luong@ou.edu>
2019-05-02 09:19:25 -0500

    Set Frame Size

--- src/HammingDist.java
+++ src/HammingDist.java
@@ -1,4 +1,23 @@
+import javax.swing.JFrame;
 
-public class HammingDist {
-
+public class HammingDist extends JFrame
+{
+	public HammingDist()
+	{
+		initFrame();
+	}
+	
+	private void initFrame() 
+	{
+		setTitle("Hamming Distance");
+		setSize(775, 775);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		HammingDist hd = new HammingDist();
+		hd.revalidate();
+	}
 }
