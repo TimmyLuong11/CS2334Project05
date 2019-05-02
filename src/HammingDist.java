7215fd0f6774c50c1f35a581efb967a0b73b9c1c
Timmy <timothy.luong@ou.edu>
2019-05-02 09:27:37 -0500

    Added Labels

--- src/HammingDist.java
+++ src/HammingDist.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 
@@ -12,19 +13,44 @@ public class HammingDist extends JFrame
 	private JButton showStation = new JButton("Show Station");
 	private JButton calHD = new JButton("Calculate HD");
 	private JButton addStation = new JButton("Add Station");
-	private JSlider slider = new JSlider(1,4);
+	private JSlider slider = new JSlider(1,4);	
+	private JLabel enterHD = new JLabel("Enter Hamming Dist:");
+	private JLabel compare = new JLabel("Compare with:");
+	private JLabel distLabel0 = new JLabel("Distance 0");
+	private JLabel disLabel1 = new JLabel("Distance 1");
+	private JLabel disLabel2 = new JLabel("Distance 2");
+	private JLabel disLabel3 = new JLabel("Distance 3");
+	private JLabel disLabel4 = new JLabel("Distance 4");
 	
 	public HammingDist()
 	{
 		initFrame();
 		setButton();
 		setSlider();
+		setText();
 		panel1.add(showStation);
 		panel1.add(calHD);
 		panel1.add(addStation);
 		panel1.add(slider);
+		panel1.add(enterHD);
+		panel1.add(compare);
+		panel1.add(distLabel0);
+		panel1.add(disLabel1);
+		panel1.add(disLabel2);
+		panel1.add(disLabel3);
+		panel1.add(disLabel4);
 		panel0.add(panel1);	
 		this.add(panel0);	
+	}
+	private void setText() 
+	{
+		enterHD.setBounds(10, 8, 120, 10);
+		compare.setBounds(10, 415, 120, 10);
+		distLabel0.setBounds(15, 500, 120, 10);
+		disLabel1.setBounds(15, 545, 120, 10);
+		disLabel2.setBounds(15, 585, 120, 10);
+		disLabel3.setBounds(15, 625, 120, 10);
+		disLabel4.setBounds(15, 665, 120, 10);
 	}
 	
 	private void setSlider() 
