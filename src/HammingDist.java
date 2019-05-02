7cff4edf0bbf5904c7478a34674c980983aa01d4
Timmy <timothy.luong@ou.edu>
2019-05-02 09:22:06 -0500

    Added Buttons

--- src/HammingDist.java
+++ src/HammingDist.java
@@ -1,11 +1,34 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class HammingDist extends JFrame
 {
+	private JPanel panel0 = new JPanel(new BorderLayout()); 
+	private JPanel panel1 = new JPanel(null); 
+	private JButton showStation = new JButton("Show Station");
+	private JButton calHD = new JButton("Calculate HD");
+	private JButton addStation = new JButton("Add Station");
+	
 	public HammingDist()
 	{
 		initFrame();
+		setButton();
+		panel1.add(showStation);
+		panel1.add(calHD);
+		panel1.add(addStation);
+		panel0.add(panel1);	
+		this.add(panel0);	
 	}
+	
+	private void setButton() 
+	{
+		showStation.setBounds(17, 100, 115, 25);
+		calHD.setBounds(17, 450, 115, 25);
+		addStation.setBounds(17, 700, 115, 25);
+	}	
 	
 	private void initFrame() 
 	{
